f6c837661d9d670e3ce01ac3a8b079b278c1fa22
Kleanthismits <kleanthismits@hotmail.gr>
2019-01-07 19:48:24 +0200

    First Commit

---
+++ kleanthis-pet-clinic/src/main/java/com/mitsioulis/kleanthispetclinic/KleanthisPetClinicApplication.java
@@ -0,0 +1,14 @@
+package com.mitsioulis.kleanthispetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KleanthisPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KleanthisPetClinicApplication.class, args);
+	}
+
+}
+
