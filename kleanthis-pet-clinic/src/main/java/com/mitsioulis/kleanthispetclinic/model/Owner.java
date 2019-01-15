9d98505e7d171f3c012a6da89d34e029cd4ddeb7
Kleanthismits <kleanthismits@hotmail.gr>
2019-01-15 14:29:01 +0200

    Adding POJOS, close #2

---
+++ kleanthis-pet-clinic/src/main/java/com/mitsioulis/kleanthispetclinic/model/Owner.java
@@ -0,0 +1,5 @@
+package com.mitsioulis.kleanthispetclinic.model;
+
+public class Owner extends Person {
+
+}
---
+++ kleanthis-pet-clinic/src/main/java/com/mitsioulis/kleanthispetclinic/model/Vet.java
@@ -0,0 +1,5 @@
+package com.mitsioulis.kleanthispetclinic.model;
+
+public class Vet extends Person {
+
+}
