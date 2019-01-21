ca0333058c01ea8c9d854462f2632f8be57d0d11
Kleanthismits <kleanthismits@hotmail.gr>
2019-01-21 12:01:36 +0200

    Implement map Based Services. closes #6

--- kleanthis-pet-clinic/pet-clinic-data/src/main/java/com/mitsioulis/kleanthispetclinic/services/OwnerService.java
+++ kleanthis-pet-clinic/pet-clinic-data/src/main/java/com/mitsioulis/kleanthispetclinic/services/OwnerService.java
@@ -1,7 +1,5 @@
 package com.mitsioulis.kleanthispetclinic.services;
 
-import java.util.Set;
-
 import com.mitsioulis.kleanthispetclinic.model.Owner;
 
 public interface OwnerService extends CrudService<Owner, Long>{
